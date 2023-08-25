14d641d2ab1dc883fb56f52d5a7def08a6f4bb8c
s.a.revin <s.a.revin@yandex.ru>
2023-08-25 15:20:39 +0300

    feat: добавить валидацию добавляемых объектов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,13 +22,36 @@ public class FilmController {
 
     @PostMapping
     private Film add(@RequestBody Film film) {
-        films.put(film.getId(), film);
+        if (films.containsKey(film.getId())) {
+            throw new ValidationException("Такой фильм уже есть в списке.");
+        }
+        if (isValid(film)) {
+            films.put(film.getId(), film);
+        }
         return film;
     }
 
     @PutMapping
     private Film update(@RequestBody Film film) {
-        films.put(film.getId(), film);
+        if (isValid(film)) {
+            films.put(film.getId(), film);
+        }
         return film;
+    }
+
+    private boolean isValid(Film film) {
+        if (film.getName().isBlank()) {
+            throw new ValidationException("Название не может быть пустым.");
+        }
+        if (film.getDescription().length() > 200) {
+            throw new ValidationException("Максимальная длина описания - 200 символов.");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года.");
+        }
+        if (film.getDuration().isNegative() || film.getDuration().isZero()) {
+            throw new ValidationException("Продолжительность фильма должна быть положительной.");
+        }
+        return true;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,13 +22,36 @@ public class UserController {
 
     @PostMapping
     private User create(@RequestBody User user) {
-        users.put(user.getId(), user);
+        if (users.containsKey(user.getId())) {
+            throw new ValidationException("Пользователь с id=" + user.getId() + " уже существует");
+        }
+        if (isValid(user)) {
+            users.put(user.getId(), user);
+        }
         return user;
     }
 
     @PutMapping
     private User update(@RequestBody User user) {
-        users.put(user.getId(), user);
+        if (isValid(user)) {
+            users.put(user.getId(), user);
+        }
         return user;
+    }
+
+    private boolean isValid(User user) {
+        if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ '@'");
+        }
+        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Логине не может быть пустым и содержать пробелы");
+        }
+        if (user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+        return true;
     }
 }
